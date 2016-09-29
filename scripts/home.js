3c76db4fd109984674166dd28ad497a8789bf61a
chokono <Andrew_Strubitskiy@mail.ru>
2016-09-29 10:53:02 +0300

    export

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,17 @@
+[].forEach.call(document.querySelectorAll('.steklo'), (e) => {
+    var t = [Math.floor(Math.random() * 220), Math.floor(Math.random() * 220), Math.floor(Math.random() * 220)];
+    e.style.backgroundColor = `rgba(${t[0]},${t[1]},${t[2]},0.30)`;
+});
 
-console.log(123);
+[].forEach.call(document.querySelectorAll('a'), (e) => {
+	e.onclick = ()=>{return false;}
+});
+
+BG.addEventListener('click', function() {
+    if (BG.style.backgroundImage === "") {
+        BG.style.backgroundImage = "url('../assets/img/land1920.png')";
+    } else {
+        BG.style.backgroundImage = "";
+    };
+});
 
