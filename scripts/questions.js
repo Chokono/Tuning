3d5df75355f9c7854dcd3c3eab13b537f7b0a69c
chokono <andrew_strubitskiy@mail.ru>
2016-10-02 16:59:07 +0300

    questions

--- scripts/questions.js
+++ scripts/questions.js
@@ -2,17 +2,22 @@ var quests = document.querySelectorAll('.vopros');
 if (quests.length) {
     [].map.call(quests, (e) => {
         e.addEventListener('click', (e) => {
-            console.log(111);
             var quest = e.target;
-            var parent_quest = quest.parentNode.parentNode.getElementsByTagName('div')[2];
-            var parent_p = quest.parentNode.parentNode.getElementsByTagName('div')[1];
-            var height = parent_quest.offsetHeight;
-            if (parent_p.style.height === height + 'px') {
-                parent_p.style.height = '0px';
-                parent_p.style.transition = '0.5s';
+            console.log (quest.parentNode.getElementsByTagName('div')[1])       
+            var parentOtvet = quest.parentNode.getElementsByTagName('div')[2];    
+            var otvet = quest.parentNode.getElementsByTagName('div')[3];
+            var parentCross = quest.parentNode.getElementsByTagName('div')[0];
+            var height = otvet.offsetHeight;
+            console.log(height);
+            console.log (parentOtvet);
+            if (parentOtvet.style.height === height + 'px') {
+                parentOtvet.style.height = '0px';
+                parentOtvet.style.transition = '0.5s';
+                parentCross.style.height = '18px';
             } else {
-                parent_p.style.height = height + 'px';
-                parent_p.style.transition = '0.5s';
+                parentOtvet.style.height = height + 'px';
+                parentOtvet.style.transition = '0.5s';
+                parentCross.style.height = '0px';
             }
         });
     })
